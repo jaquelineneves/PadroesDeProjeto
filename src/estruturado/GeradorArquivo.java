496d04ff4a1389f602a9dec91a1e9b3c17e5bfbd
ADMIN-PC\ADMIN <jaque.nevees2@gmail.com>
2019-03-20 23:36:14 -0300

    Refatorando - Criando a Classe ProcessaXmlCompactado

--- src/estruturado/GeradorArquivo.java
+++ src/estruturado/GeradorArquivo.java
@@ -1,20 +1,19 @@
 package estruturado;
 
-import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Map;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 public class GeradorArquivo {
-
+	//Ctrl + Shift + O retira imports desnecessarios
 	public final void gerarArquivo(String nome, Map<String, Object> propriedades, String tipo) throws IOException {
 		byte[] bytes = null;
 		if (tipo.equals("PROPRIEDADES_CRIPTOGRAFADO")) {
-			bytes = processaPropriedadesCriptografado(propriedades);
+			ProcessaPropriedadesCriptografado propCript = new ProcessaPropriedadesCriptografado();
+			bytes = propCript.processaPropriedadesCriptografado(propriedades);
 		} else if (tipo.equals("XML_COMPACTADO")) {
-			bytes = processaXmlCompactado(propriedades);
+			ProcessaXmlCompactado xml = new ProcessaXmlCompactado();
+			bytes = xml.processaXmlCompactado(propriedades);
 		} else {
 			System.out.println("Desconheço essa opção");
 			return;
@@ -22,33 +21,6 @@ public class GeradorArquivo {
 		FileOutputStream fileout = new FileOutputStream(nome);
 		fileout.write(bytes);
 		fileout.close();		
-	}
-	
-	private byte[] processaXmlCompactado(Map<String, Object> propriedades) throws IOException {
-		String conteudo = geraConteudoXml(propriedades);
-		return processaCompactacao(conteudo.getBytes());
-	}
-	
-	private String geraConteudoXml(Map<String, Object> propriedades) {
-		//gera xml
-		StringBuilder propFileBuilder = new StringBuilder();
-		propFileBuilder.append("<properties>");
-		for (String prop: propriedades.keySet()) {
-			propFileBuilder.append("<"+prop+">"+propriedades.get(prop)+"</"+prop+">");
-		}
-		propFileBuilder.append("</propriedades>");
-		return propFileBuilder.toString();	
-	}
-	
-	private byte[] processaCompactacao(byte[] bytes) throws IOException {
-		//compacta
-		ByteArrayOutputStream byteout = new ByteArrayOutputStream();
-		ZipOutputStream out = new ZipOutputStream(byteout);
-		out.putNextEntry(new ZipEntry("internal"));
-		out.write(bytes);
-		out.closeEntry();
-		out.close();
-		return byteout.toByteArray();	
 	}
 
 }
