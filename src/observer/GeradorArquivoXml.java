7546e7d01af3b31bb76c426480cfbc03e00511d2
ADMIN-PC\ADMIN <jaque.nevees2@gmail.com>
2019-03-22 00:18:03 -0300

    Copiando o projeto e criando o pacote do observer

---
+++ src/observer/GeradorArquivoXml.java
@@ -0,0 +1,26 @@
+package observer;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class GeradorArquivoXml extends GeradorArquivo {
+	
+	public GeradorArquivoXml(Processador processador) {
+		super(processador);
+	}
+	@Override
+	protected String gerarConteudo(Map<String, Object> propriedades) {
+		//gera xml
+		StringBuilder propFileBuilder = new StringBuilder();
+		propFileBuilder.append("<properties>");
+		for (String prop: propriedades.keySet()) {
+			propFileBuilder.append("<"+prop+">"+propriedades.get(prop)+"</"+prop+">");
+		}
+		propFileBuilder.append("</propriedades>");
+		return propFileBuilder.toString();	
+	}
+	
+}
---
+++ src/observer/Processador.java
@@ -0,0 +1,7 @@
+package observer;
+
+import java.io.IOException;
+
+public interface Processador {
+	public byte[] processaConteudo(byte[] bytes) throws IOException;
+}
